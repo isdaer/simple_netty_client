3a1430d28d1bf9abc6e65affb8e8ab1ed11620f2
chenhui <chenhui@zeaho.com>
2020-06-16 17:53:06 +0800

    format code

--- src/main/java/com/zeaho/TCP/service/impl/TCPServiceImpl.java
+++ src/main/java/com/zeaho/TCP/service/impl/TCPServiceImpl.java
@@ -6,7 +6,6 @@ import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.logging.LoggingHandler;
@@ -20,20 +19,20 @@ public class TCPServiceImpl implements TCPService {
         NioEventLoopGroup group = new NioEventLoopGroup();
         try {
             Bootstrap bootstrap = new Bootstrap();
-            bootstrap.group(group);
-            bootstrap.channel(NioSocketChannel.class);
-            bootstrap.option(ChannelOption.SO_KEEPALIVE, true);
-            bootstrap.handler(new ChannelInitializer() {
-                @Override
-                protected void initChannel(Channel ch) throws Exception {
-                    ch.pipeline().addLast("logging", new LoggingHandler("DEBUG"));
-                    ch.pipeline().addLast(new EchoHandler());
-                }
-            });
-            bootstrap.remoteAddress(ip, port);
-            ChannelFuture future = bootstrap.connect().sync();
+            bootstrap.
+                    group(group).
+                    channel(NioSocketChannel.class).
+                    remoteAddress(ip, port)
+                    .handler(new ChannelInitializer() {
+                        @Override
+                        protected void initChannel(Channel ch) throws Exception {
+                            ch.pipeline().addLast("logging", new LoggingHandler("DEBUG"));
+                            ch.pipeline().addLast(new EchoHandler());
+                        }
+                    });
 
-            future.channel().closeFuture().sync();
+            ChannelFuture future = bootstrap.connect().sync();
+            //future.channel().closeFuture().sync();阻塞
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
