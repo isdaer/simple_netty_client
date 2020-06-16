c4997b54877a3250846d033f12f025d5f4912a7f
chenhui <chenhui@zeaho.com>
2020-06-16 18:18:26 +0800

    add tcp schedule

--- src/main/java/com/zeaho/TCP/schedule/TCPInit.java
+++ src/main/java/com/zeaho/TCP/schedule/TCPInit.java
@@ -1,36 +1,32 @@
-package com.zeaho.TCP.controller;
+package com.zeaho.TCP.schedule;
 
 import com.zeaho.TCP.service.TCPService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-@RestController
-@EnableScheduling   // 2.开启定时任务
-public class TCPController {
+@EnableScheduling  //定时任务
+public class TCPInit {
 
     @Value("${ip}")
-    private String ip;
+    static private String ip;
 
     @Value("${port}")
-    private int port;
+    static private int port;
 
     @Autowired
     private TCPService tcpService;
 
-    @Scheduled(fixedRate=10000)
+    @Scheduled(fixedRate = 30000)
     public void init() {
         System.out.println("定时任务执行");
         try {
             tcpService.init(ip, port);
-            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
-            System.out.println(df.format(new Date()));// new Date()为获取当前系统时间
+
             tcpService.init(ip, port);
             SimpleDateFormat df2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
             System.out.println(df2.format(new Date()));// new Date()为获取当前系统时间
