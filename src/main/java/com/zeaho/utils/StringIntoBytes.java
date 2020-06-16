a13341a6e188d0124d02d120817440d44716ddf6
chenhui <chenhui@zeaho.com>
2020-06-16 16:49:49 +0800

    add StringIntoBytes util

---
+++ src/main/java/com/zeaho/utils/StringIntoBytes.java
@@ -0,0 +1,13 @@
+package com.zeaho.utils;
+
+import java.util.ArrayList;
+
+//将传入的字符串转化为字节追加进传入的字节数组
+public class StringIntoBytes {
+    public static void intoBytes(ArrayList<Byte> bytes, String s) {
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            bytes.add((byte) c);
+        }
+    }
+}
