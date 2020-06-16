ed6d8035fe619162350c9d75a52f5f37d78d9326
chenhui <chenhui@zeaho.com>
2020-06-16 16:31:47 +0800

    project-init

---
+++ src/main/java/com/zeaho/ShhkApiApplication.java
@@ -0,0 +1,13 @@
+package com.zeaho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShhkApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShhkApiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/zeaho/controller/TCPController.java
@@ -0,0 +1,13 @@
+package com.zeaho.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TCPController {
+
+    @PostMapping("/tcp/send")
+    public String tcpSend() {
+        return "111";
+    }
+}
