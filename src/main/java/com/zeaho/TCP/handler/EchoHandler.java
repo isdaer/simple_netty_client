453c3d7af6235649071ca48fd64c3e74f1678ead
chenhui <chenhui@zeaho.com>
2020-06-17 14:51:46 +0800

    format code

--- src/main/java/com/zeaho/TCP/handler/EchoHandler.java
+++ src/main/java/com/zeaho/TCP/handler/EchoHandler.java
@@ -14,80 +14,12 @@ public class EchoHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
 
-
-//        ArrayList<Byte> bytes = new ArrayList<>();
-//
-//        //起始符
-//        String header = "##";
-//        StringIntoBytes.intoBytes(bytes, header);
-//
-//        //命令单元
-//        bytes.add((byte) 0x02);
-//
-//        //车辆识别号
-//        //String headingCode = "HCM1HH00L00036634";
-//        String headingCode = "0000SY005EBK85008";
-//        StringIntoBytes.intoBytes(bytes, headingCode);
-//
-//        //终端软件版本号
-//        bytes.add((byte) '3');
-//
-//        //数据加密方式
-//        bytes.add((byte) 0x01);
-//
-//
-//        /**----------------------拼接数据----------------------*/
-//        ArrayList<Byte> addBytes = new ArrayList<>();
-//        //数据采集时间
-//        addBytes.add((byte) 20);//年
-//        addBytes.add((byte) 6);//月
-//        addBytes.add((byte) 3);//日
-//        addBytes.add((byte) 12);//时
-//        addBytes.add((byte) 0);//分
-//        addBytes.add((byte) 0);//秒
-//        //信息类型标志
-//        addBytes.add((byte) 0x87);//国三及以下数据流
-//        //信息流水号
-//        addBytes.add((byte) 0);
-//        addBytes.add((byte) 1);
-//        /**------------信息体------------*/
-//        //油箱液位
-//        addBytes.add((byte) 100);
-//        //定位状态
-//        addBytes.add((byte) 0);
-//        //精度
-//        addBytes.add((byte) 0x42);
-//        addBytes.add((byte) 0xF3);
-//        addBytes.add((byte) 0x0C);
-//        addBytes.add((byte) 0x4A);
-//        //纬度
-//        addBytes.add((byte) 0x41);
-//        addBytes.add((byte) 0xF7);
-//        addBytes.add((byte) 0xfb);
-//        addBytes.add((byte) 0x0B);
-//        //工作状态
-//        addBytes.add((byte) 0x02);
-//        /**------------信息体------------*/
-//        /**----------------------拼接数据----------------------*/
-//
-//        //计算数据长度
-//        int addBytesSize = addBytes.size();
-//
-//        //追加数据长度
-//        bytes.add((byte) 0);
-//        bytes.add((byte) addBytesSize);
-//
-//        //追加数据
-//        for (byte b : addBytes) {
-//            bytes.add(b);
-//        }
-
         JointBytes jointBytes = new JointBytes();
         ArrayList<Byte> bytes = jointBytes.JointBytes();
 
         //结果转换
         int size = bytes.size();
-        byte[] by = new byte[size + 1];
+        byte[] by = new byte[size + 1];//多一位用于存储BBC
         byte[] xors = new byte[size - 2];//去除前两位计算BCC
 
         for (int i = 0; i < size; i++) {
@@ -103,6 +35,4 @@ public class EchoHandler extends ChannelInboundHandlerAdapter {
 
         ctx.writeAndFlush(Unpooled.copiedBuffer(by));
     }
-
-
 }
