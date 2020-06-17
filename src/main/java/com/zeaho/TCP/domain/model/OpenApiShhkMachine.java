bcef69339bf32e84657987ba9948e0ccc1eb6cd2
chenhui <chenhui@zeaho.com>
2020-06-17 10:57:42 +0800

    create open_api_shhk_machine_model

--- src/main/java/com/zeaho/TCP/domain/model/OpenApiShhkMachine.java
+++ src/main/java/com/zeaho/TCP/domain/model/OpenApiShhkMachine.java
@@ -1,4 +1,133 @@
 package com.zeaho.TCP.domain.model;
 
-public class OpenApiShhkMachine {
+import java.util.Date;
+
+public class OpenApiShhkMachine extends AbstractModel {
+
+    private Date installAt;
+    private String deviceSn;
+    private String sensorSn;
+    private String environmentCode;
+    private String emissionStandard;
+    private String category;
+    private String brand;
+    private String machineModel;
+    private String machineCode;
+    private String ownerName;
+    private String ownerPhone;
+    private Long deviceId;
+    private Long tenantId;
+    private Long machineId;
+
+    public Date getInstallAt() {
+        return installAt;
+    }
+
+    public void setInstallAt(Date installAt) {
+        this.installAt = installAt;
+    }
+
+    public String getDeviceSn() {
+        return deviceSn;
+    }
+
+    public void setDeviceSn(String deviceSn) {
+        this.deviceSn = deviceSn;
+    }
+
+    public String getSensorSn() {
+        return sensorSn;
+    }
+
+    public void setSensorSn(String sensorSn) {
+        this.sensorSn = sensorSn;
+    }
+
+    public String getEnvironmentCode() {
+        return environmentCode;
+    }
+
+    public void setEnvironmentCode(String environmentCode) {
+        this.environmentCode = environmentCode;
+    }
+
+    public String getEmissionStandard() {
+        return emissionStandard;
+    }
+
+    public void setEmissionStandard(String emissionStandard) {
+        this.emissionStandard = emissionStandard;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getMachineModel() {
+        return machineModel;
+    }
+
+    public void setMachineModel(String machineModel) {
+        this.machineModel = machineModel;
+    }
+
+    public String getMachineCode() {
+        return machineCode;
+    }
+
+    public void setMachineCode(String machineCode) {
+        this.machineCode = machineCode;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public String getOwnerPhone() {
+        return ownerPhone;
+    }
+
+    public void setOwnerPhone(String ownerPhone) {
+        this.ownerPhone = ownerPhone;
+    }
+
+    public Long getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(Long deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public Long getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(Long tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public Long getMachineId() {
+        return machineId;
+    }
+
+    public void setMachineId(Long machineId) {
+        this.machineId = machineId;
+    }
 }
