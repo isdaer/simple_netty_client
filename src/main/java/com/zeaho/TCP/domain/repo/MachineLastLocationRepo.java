5f5f7c865996143abe9f803ca0f5d92bf3f15175
chenhui <chenhui@zeaho.com>
2020-06-17 14:44:56 +0800

    create machine_lastLocation_repo

---
+++ src/main/java/com/zeaho/TCP/domain/repo/MachineLastLocationRepo.java
@@ -0,0 +1,8 @@
+package com.zeaho.TCP.domain.repo;
+
+import com.zeaho.TCP.domain.model.MachineLastLocation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface MachineLastLocationRepo extends JpaRepository<MachineLastLocation, Long>, JpaSpecificationExecutor<MachineLastLocation> {
+}
